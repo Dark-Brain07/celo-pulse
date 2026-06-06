bdbf920add2e53a8eaf2b5beb1b4c42f89820f4b
Dark-Brain07 <85172976+Dark-Brain07@users.noreply.github.com>
2026-06-06 20:40:47 +0600

    chore(number): add strict numeric type guard

--- src/lib/utils/number.ts
+++ src/lib/utils/number.ts
@@ -2,3 +2,4 @@ export const random = (min: number, max: number): number => Math.floor(Math.rand
 export const clamp = (val: number, min: number, max: number): number => Math.min(Math.max(val, min), max);
 export const inRange = (num: number, min: number, max: number): boolean => num >= min && num <= max;
 export const round = (num: number, decimals: number = 0): number => Number(Math.round(Number(num + 'e' + decimals)) + 'e-' + decimals);
+export const isNumber = (val: any): val is number => typeof val === 'number' && !isNaN(val);
