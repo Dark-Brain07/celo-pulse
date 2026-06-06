1e453e08c1d313adf085686f5329440b52e08e88
Dark-Brain07 <85172976+Dark-Brain07@users.noreply.github.com>
2026-06-06 20:41:06 +0600

    feat(validation): create IPv4 string check logic

--- src/lib/utils/validation.ts
+++ src/lib/utils/validation.ts
@@ -6,3 +6,4 @@ export const isAlpha = (str: string): boolean => /^[A-Za-z]+$/.test(str);
 export const isNumeric = (str: string): boolean => /^\d+$/.test(str);
 export const isAlphaNumeric = (str: string): boolean => /^[A-Za-z0-9]+$/.test(str);
 export const isUuid = (str: string): boolean => /^[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}$/i.test(str);
+export const isIpAddress = (str: string): boolean => /^(?:[0-9]{1,3}\.){3}[0-9]{1,3}$/.test(str);
