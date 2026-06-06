cafbc0c5e887b2fb7c29e80861411caf964948d3
Dark-Brain07 <85172976+Dark-Brain07@users.noreply.github.com>
2026-06-06 20:41:11 +0600

    chore(validation): wrap strict function type guard

--- src/lib/utils/validation.ts
+++ src/lib/utils/validation.ts
@@ -15,3 +15,4 @@ export const isJSON = (str: string): boolean => { try { JSON.parse(str); return 
 export const isStrongPassword = (str: string): boolean => str.length >= 8 && /[A-Z]/.test(str) && /[0-9]/.test(str) && /[^A-Za-z0-9]/.test(str);
 export const isBoolean = (val: any): val is boolean => typeof val === 'boolean';
 export const isSymbol = (val: any): val is symbol => typeof val === 'symbol';
+export const isFunction = (val: any): val is Function => typeof val === 'function';
