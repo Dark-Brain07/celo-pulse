e420520c843ebccd0bc6c23b66a2b6fd7f7df4b7
Dark-Brain07 <85172976+Dark-Brain07@users.noreply.github.com>
2026-06-06 20:40:19 +0600

    feat(string): add uppercase text converter

--- src/lib/utils/string.ts
+++ src/lib/utils/string.ts
@@ -1,2 +1,3 @@
 export const capitalize = (str: string): string => str.charAt(0).toUpperCase() + str.slice(1);
 export const lowercase = (str: string): string => str.toLowerCase();
+export const uppercase = (str: string): string => str.toUpperCase();
