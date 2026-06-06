a030d2b5de5445eea37f62408836ace33a06d33d
Dark-Brain07 <85172976+Dark-Brain07@users.noreply.github.com>
2026-06-06 20:40:19 +0600

    chore(string): add type guard for string type

--- src/lib/utils/string.ts
+++ src/lib/utils/string.ts
@@ -1,3 +1,4 @@
 export const capitalize = (str: string): string => str.charAt(0).toUpperCase() + str.slice(1);
 export const lowercase = (str: string): string => str.toLowerCase();
 export const uppercase = (str: string): string => str.toUpperCase();
+export const isString = (val: any): val is string => typeof val === 'string';
