7e73e169e7656c4e8ec552be870ae645a0f606de
Dark-Brain07 <85172976+Dark-Brain07@users.noreply.github.com>
2026-06-06 20:41:04 +0600

    feat(validation): add hex color code validator

--- src/lib/utils/validation.ts
+++ src/lib/utils/validation.ts
@@ -1,3 +1,4 @@
 export const isEmail = (str: string): boolean => /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(str);
 export const isUrl = (str: string): boolean => /^https?:\/\/[\w\-]+(\.[\w\-]+)+[/#?]?.*$/.test(str);
 export const isHex = (str: string): boolean => /^[0-9A-Fa-f]+$/.test(str);
+export const isHexColor = (str: string): boolean => /^#([0-9A-Fa-f]{3}|[0-9A-Fa-f]{6})$/.test(str);
