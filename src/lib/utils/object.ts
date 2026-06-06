9d12d3d14d08a63c37fdae310411e79511f16a0b
Dark-Brain07 <85172976+Dark-Brain07@users.noreply.github.com>
2026-06-06 20:40:42 +0600

    chore(object): add object immutability wrapper

--- src/lib/utils/object.ts
+++ src/lib/utils/object.ts
@@ -11,3 +11,4 @@ export const size = (obj: object): number => Object.keys(obj).length;
 export const isEmptyObject = (obj: object): boolean => Object.keys(obj).length === 0;
 export const has = <T extends object>(obj: T, key: PropertyKey): boolean => Object.prototype.hasOwnProperty.call(obj, key);
 export const cleanObject = <T extends object>(obj: T): Partial<T> => Object.fromEntries(Object.entries(obj).filter(([_, v]) => v !== undefined)) as Partial<T>;
+export const freeze = <T extends object>(obj: T): Readonly<T> => Object.freeze(obj);
