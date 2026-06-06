739484d7bc75dae860b6f991696eedf6abb72e9c
Dark-Brain07 <85172976+Dark-Brain07@users.noreply.github.com>
2026-06-06 20:40:55 +0600

    feat(date): add Unix timestamp converter

--- src/lib/utils/date.ts
+++ src/lib/utils/date.ts
@@ -1,2 +1,3 @@
 export const now = (): number => Date.now();
 export const isDate = (val: any): val is Date => val instanceof Date && !isNaN(val.getTime());
+export const toUnix = (date: Date): number => Math.floor(date.getTime() / 1000);
