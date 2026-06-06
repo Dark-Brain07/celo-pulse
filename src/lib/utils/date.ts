bbd6c288e1f0fc83ff0c0124e5c3be64b92bf343
Dark-Brain07 <85172976+Dark-Brain07@users.noreply.github.com>
2026-06-06 20:40:56 +0600

    feat(date): create date constructor from Unix time

--- src/lib/utils/date.ts
+++ src/lib/utils/date.ts
@@ -1,3 +1,4 @@
 export const now = (): number => Date.now();
 export const isDate = (val: any): val is Date => val instanceof Date && !isNaN(val.getTime());
 export const toUnix = (date: Date): number => Math.floor(date.getTime() / 1000);
+export const fromUnix = (unix: number): Date => new Date(unix * 1000);
