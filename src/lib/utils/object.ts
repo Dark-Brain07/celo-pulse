cd38d3ef8989de8100163b788be3adeef46eba05
Dark-Brain07 <85172976+Dark-Brain07@users.noreply.github.com>
2026-06-06 20:40:38 +0600

    feat(object): wrap Object.entries with strict types

--- src/lib/utils/object.ts
+++ src/lib/utils/object.ts
@@ -1,3 +1,4 @@
 export const isObject = (val: any): val is Record<string, any> => val !== null && typeof val === 'object' && !Array.isArray(val);
 export const keys = <T extends object>(obj: T): (keyof T)[] => Object.keys(obj) as (keyof T)[];
 export const values = <T extends object>(obj: T): T[keyof T][] => Object.values(obj) as T[keyof T][];
+export const entries = <T extends object>(obj: T): [keyof T, T[keyof T]][] => Object.entries(obj) as [keyof T, T[keyof T]][];
