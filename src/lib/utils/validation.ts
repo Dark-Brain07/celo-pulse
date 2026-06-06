8088bf0abe79c58ca5ad4675b402e21e053381b3
Dark-Brain07 <85172976+Dark-Brain07@users.noreply.github.com>
2026-06-06 20:41:05 +0600

    feat(validation): add alphanumeric format checker

--- src/lib/utils/validation.ts
+++ src/lib/utils/validation.ts
@@ -4,3 +4,4 @@ export const isHex = (str: string): boolean => /^[0-9A-Fa-f]+$/.test(str);
 export const isHexColor = (str: string): boolean => /^#([0-9A-Fa-f]{3}|[0-9A-Fa-f]{6})$/.test(str);
 export const isAlpha = (str: string): boolean => /^[A-Za-z]+$/.test(str);
 export const isNumeric = (str: string): boolean => /^\d+$/.test(str);
+export const isAlphaNumeric = (str: string): boolean => /^[A-Za-z0-9]+$/.test(str);
