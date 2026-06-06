48794c794ca473ca26f738cb91c9999131d33964
Dark-Brain07 <85172976+Dark-Brain07@users.noreply.github.com>
2026-06-06 20:41:04 +0600

    feat(validation): create hex string validation

--- src/lib/utils/validation.ts
+++ src/lib/utils/validation.ts
@@ -1,2 +1,3 @@
 export const isEmail = (str: string): boolean => /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(str);
 export const isUrl = (str: string): boolean => /^https?:\/\/[\w\-]+(\.[\w\-]+)+[/#?]?.*$/.test(str);
+export const isHex = (str: string): boolean => /^[0-9A-Fa-f]+$/.test(str);
